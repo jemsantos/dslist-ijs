40b7c749a615089bd17ce143c6aec692f7d2a8dc
José Eustaquio Muniz Santos <eustaquio.cadastros@gmail.com>
2023-05-13 18:05:17 -0300

    Domain model implements

--- src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
+++ src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
@@ -4,6 +4,7 @@
 package com.devsuperior.dslist.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.dslist.entities.Game;
 
